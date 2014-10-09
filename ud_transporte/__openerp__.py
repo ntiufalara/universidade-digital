6f44fdd18b192a3787c438788f1f4140f4878770
diogo <diogo.silva@arapiraca.ufal.br>
2014-10-09 11:38:42 -0300

    Adicionado módulo transporte

---
+++ ud_transporte/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Transporte (UD)",
+    "version": "1.1",
+    "depends": ["base",'ud'],
+    "author": "LaPEC",
+    "category": "Universidade Digital",
+    "description": """
+    Módulo para gerenciamente de transporte universitário
+    """,
+    'data' : [
+              'ud_transporte_motorista_view.xml',
+              'ud_transporte_veiculo_view.xml',
+              'ud_transporte_manutencao_view.xml',
+              'ud_transporte_solicitacao_view.xml',
+              'ud_transporte_viagem_view.xml',
+              'wizards/concluir_viagem_view.xml',
+              ],
+    'installable': True,
+    'active': False,
+#    'certificate': 'certificate',
+}
---
+++ ud_transporte/wizards/__init__.py
@@ -0,0 +1,1 @@
+import concluir_viagem
