27ec3f3f22e1ebf9b8c010873548a3b5b85de2e9
Cloves Oliveira <clovesolivier@hotmail.com>
2017-02-19 14:51:10 -0300

    Correção da criação do usuário. Agora o login só é validado por CPF se seguir seu padrão.

--- openerp/addons/ud/usuario/usuario.py
+++ openerp/addons/ud/usuario/usuario.py
@@ -39,11 +39,9 @@ class UsuarioUD(osv.Model):
             vals["tz"] = vals.get("tz", "America/Maceio")
             pessoa_model = self.pool.get("ud.employee")
             cpf = vals.get("login", False)
-            if cpf:
+            combinacao = self.combinacao_cpf(cpf)
+            if cpf and combinacao:
                 vals["login"] = cpf.replace(".", "").replace("-", "")
-                combinacao = self.valida_cpf(cpf)
-                if not combinacao:
-                    raise osv.except_osv(u"CPF/Login inválido", u"O CPF/login inserido não é válido.")
                 combinacao = combinacao.groups()
                 cpf = ".".join(combinacao[:-1]) + "-" + combinacao[-1]
                 pessoa = pessoa_model.search(cr, SUPERUSER_ID, [("cpf", "=", cpf)])
@@ -51,26 +49,25 @@ class UsuarioUD(osv.Model):
                     pessoa = pessoa_model.browse(cr, SUPERUSER_ID, pessoa[0], context)
                     if pessoa.user_id:
                         raise osv.except_osv(u"Usuário existente", u"Existe outro usuário cadastrado com seu CPF.")
-            else:
-                raise osv.except_osv(u"Dados inválidos", u"Não é possível criar um usuário sem informar um CPF/Login.")
             res = super(UsuarioUD, self).create(cr, uid, vals, context)
-            if pessoa and not getattr(pessoa, "user_id", None):
-                dados = {"user_id": res}
-                if "name" in vals:
-                    dados["name"] = vals["name"]
-                pessoa.write(dados)
-            else:
-                dados = {
-                    "name": vals.get("name", False), "cpf": cpf, "work_email": vals.get("email", False), "user_id": res,
-                }
-                pessoa_model.create(cr, SUPERUSER_ID, dados, context)
-            try:
+            if combinacao:
+                if pessoa:
+                    dados = {}
+                    if not getattr(pessoa, "user_id", None):
+                        dados = {"user_id": res}
+                    if "name" in vals:
+                        dados["name"] = vals["name"]
+                    if dados:
+                        pessoa.write(dados)
+                else:
+                    dados = {
+                        "name": vals.get("name", False), "cpf": cpf, "work_email": vals.get("email", False), "user_id": res,
+                    }
+                    pessoa_model.create(cr, SUPERUSER_ID, dados, context)
                 group = self.pool.get("ir.model.data").get_object(
                     cr, SUPERUSER_ID, "base", "usuario_ud", context
                 )
                 group.write({"users": [(4, res)]})
-            except ValueError:
-                pass
             return res
         return super(UsuarioUD, self).create(cr, uid, vals, context)
 
@@ -79,55 +76,66 @@ class UsuarioUD(osv.Model):
             pessoa_model = self.pool.get("ud.employee")
             cpf = vals.get("login", False)
             if cpf:
-                if len(ids) > 1:
-                    raise osv.except_osv(u"Repetição de Login",
-                                         u"Não é permitido repetir o mesmo login para múltiplos usuários.")
-                p_atual = pessoa_model.search(cr, SUPERUSER_ID, [("user_id", "=", ids[0])], context=context)
-                if len(p_atual) > 1:
-                    raise osv.except_osv(u"Multiplos usuários",
-                                         u"Há multiplos registros de pessoa vinculado ao mesmo usuário.")
-                vals["login"] = cpf.replace(".", "").replace("-", "")
-                combinacao = self.valida_cpf(cpf)
-                if not combinacao:
-                    raise osv.except_osv(u"CPF/Login inválido", u"O CPF/login inserido não é válido.")
-                combinacao = combinacao.groups()
-                cpf = ".".join(combinacao[:-1]) + "-" + combinacao[-1]
-                pessoa = pessoa_model.search(cr, SUPERUSER_ID, [("cpf", "=", cpf)], context=context)
-                if pessoa:
-                    pessoa = pessoa_model.browse(cr, SUPERUSER_ID, pessoa[0], context)
-                    p_atual = p_atual and p_atual[0] or False
-                    if p_atual and p_atual != pessoa.id:
-                        raise osv.except_osv("Conflito",
-                                             u"O CPF digitado está vinculado a um registro de pessoa de outro usuário.")
-                    elif not pessoa.user_id:
-                        dados = {"user_id": ids[0]}
+                combinacao = self.combinacao_cpf(cpf)
+                if combinacao:
+                    if len(ids) > 1:
+                        raise osv.except_osv(u"Repetição de Login",
+                                             u"Não é permitido repetir o mesmo login para múltiplos usuários.")
+                    pessoa_atual = pessoa_model.search(cr, SUPERUSER_ID, [("user_id", "in", ids)], context=context)
+                    if len(pessoa_atual) > 1:
+                        raise osv.except_osv(u"Multiplos usuários",
+                                             u"Há multiplos registros de pessoa vinculado ao mesmo usuário.")
+                    pessoa_atual = pessoa_atual and pessoa_atual[0] or False
+                    vals["login"] = cpf.replace(".", "").replace("-", "")
+                    combinacao = combinacao.groups()
+                    cpf = ".".join(combinacao[:-1]) + "-" + combinacao[-1]
+                    pessoa = pessoa_model.search(cr, SUPERUSER_ID, [("cpf", "=", cpf)], context=context)
+                    if pessoa:
+                        pessoa = pessoa_model.browse(cr, SUPERUSER_ID, pessoa[0], context)
+                        dados = {}
                         if "name" in vals:
                             dados["name"] = vals["name"]
-                        pessoa.write(dados)
-                elif p_atual:
-                    p_atual = pessoa_model.browse(cr, SUPERUSER_ID, p_atual[0], context)
-                    if p_atual.cpf != cpf:
-                        p_atual.write({"cpf": cpf})
-                else:
-                    usuario = self.browse(cr, uid, ids[0], context)
-                    dados = {
-                        "name": usuario.name, "cpf": cpf, "work_email": usuario.email, "user_id": ids[0],
-                    }
-                    pessoa_model.create(cr, SUPERUSER_ID, dados, context)
-                    try:
-                        group = self.pool.get("ir.model.data").get_object(
-                            cr, SUPERUSER_ID, "base", "usuario_ud", context
-                        )
-                        group.write({"users": [(4, ids[0])]})
-                    except ValueError:
-                        pass
+                        if not pessoa.user_id:
+                            dados["user_id"] = ids[0]
+                            pessoa.write(dados)
+                        elif pessoa_atual == pessoa.id:
+                            pessoa.write(dados)
+                        elif pessoa_atual:
+                            raise osv.except_osv(
+                                u"Conflito", u"O CPF digitado está vinculado a um registro de pessoa de outro usuário."
+                            )
+                    elif pessoa_atual:
+                        pessoa_atual = pessoa_model.browse(cr, SUPERUSER_ID, pessoa_atual, context)
+                        dados = {}
+                        if "name" in vals:
+                            dados["name"] = vals["name"]
+                        if pessoa_atual.cpf != cpf:
+                            dados["cpf"] = cpf
+                            pessoa_atual.write(dados)
+                    else:
+                        usuario = self.browse(cr, uid, ids[0], context)
+                        dados = {
+                            "name": usuario.name, "cpf": cpf, "work_email": usuario.email, "user_id": ids[0],
+                        }
+                        pessoa_model.create(cr, SUPERUSER_ID, dados, context)
+                    group = self.pool.get("ir.model.data").get_object(
+                        cr, SUPERUSER_ID, "base", "usuario_ud", context
+                    )
+                    group.write({"users": [(4, ids[0])]})
             elif vals.get("name", False):
-                pessoas = pessoa_model.search(cr, SUPERUSER_ID, [("user_id", "in", ids)], context=context)
-                pessoa_model.write(cr, SUPERUSER_ID, pessoas, {"name": vals["name"]}, context)
+                pessoa_atual = pessoa_model.search(cr, SUPERUSER_ID, [("user_id", "in", ids)], context=context)
+                pessoa_model.write(cr, SUPERUSER_ID, pessoa_atual, {"name": vals["name"]}, context)
         return super(UsuarioUD, self).write(cr, uid, ids, vals, context)
 
+    def combinacao_cpf(self, cpf):
+        return re.match("(\d{3})\.?(\d{3})\.?(\d{3})-?(\d\d)", cpf or "")
+
     def valida_cpf(self, cpf):
-        combinacao = re.match("(\d{3})\.?(\d{3})\.?(\d{3})-?(\d\d)", cpf or "")
+        if not isinstance(cpf, str):
+            combinacao = cpf
+            cpf = cpf.string
+        else:
+            combinacao = self.combinacao_cpf(cpf)
         if not combinacao:
             return False
         elif cpf.count(cpf[0]) == 11:
@@ -143,5 +151,17 @@ class UsuarioUD(osv.Model):
         cpf_r = cpf.replace(".", "").replace("-", "")
         if int(cpf[-2]) != calcula_dv(cpf_r) or int(cpf[-1]) != calcula_dv(cpf_r, -1):
             return False
-        return combinacao
+        return True
+
+    def _valida_cpf(self, cr, uid, ids, context=None):
+        for user in self.browse(cr, uid, ids, context):
+            combinacao = self.combinacao_cpf(user.login)
+            if combinacao:
+                if not self.valida_cpf(combinacao):
+                    return False
+        return True
+
+    _constraints = [
+        (_valida_cpf, u"O CPF/login inserido não é válido.", [u"CPF/Login inválido"])
+    ]
 
