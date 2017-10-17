d3ae9dc5851c267a4f433c0f7ea09e7d631c023f
Diogo <diogocs2@gmail.com>
2017-10-17 17:26:31 -0300

    Corrigindo erro no Modelo Espaco, (virgulas no final das linhas). Concluíndo formulário de cadastro de espaço. TODO: Realizar importação dos dados do servidor atual

--- odoo/addons/ud/models/Espaco.py
+++ odoo/addons/ud/models/Espaco.py
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Espaco(models.Model):
@@ -8,22 +8,22 @@ class Espaco(models.Model):
     """
     _name = 'ud.espaco'
 
-    name = fields.Char(u'Nome', required=True),
-    # capacidade = fields.Integer(u'Capacidade', required=True, help=u"Número de pessoas."),
-    # permite_reserva = fields.Boolean(u'Permitir Reserva'),
-    # campus_id = fields.Many2one('ud.campus', u'Campus', required=True),
-    # polo_id = fields.Many2one('ud.polo', u'Polo', required=True, ondelete='cascade'),
-    # bloco_id = fields.Many2one('ud.bloco', u'Bloco', required=True, ondelete='cascade'),
-    # informacoes_adicionais = fields.Text(u'Descrição'),
-    # responsavel_ids = fields.Many2many('res.users', 'ud_espaco_responsavel', string=u'Responsável')
+    name = fields.Char(u'Nome', required=True)
+    capacidade = fields.Integer(u'Capacidade', required=True, help=u"Número de pessoas.")
+    permite_reserva = fields.Boolean(u'Permitir Reserva')
+    campus_id = fields.Many2one('ud.campus', u'Campus', required=True)
+    polo_id = fields.Many2one('ud.polo', u'Polo', required=True, ondelete='cascade')
+    bloco_id = fields.Many2one('ud.bloco', u'Bloco', required=True, ondelete='cascade')
+    informacoes_adicionais = fields.Text(u'Descrição')
+    responsavel_ids = fields.Many2many('res.users', 'ud_espaco_responsavel', string=u'Responsável')
 
-    # @api.onchange('polo_id')
-    # def limpa_bloco(self, cr, uid, ids):
-    #     """
-    #     Limpa o campo bloco se o polo não pertencer a ele.
-    #     """
-    #     campos = ["polo_id"]
-    #     valores = {}
-    #     for campo in campos:
-    #         valores[campo] = ""
-    #     return {'value': valores}
+    @api.onchange('polo_id')
+    def limpa_bloco(self):
+        """
+        Limpa o campo bloco se o polo não pertencer a ele.
+        """
+        campos = ["polo_id"]
+        valores = {}
+        for campo in campos:
+            valores[campo] = ""
+        return {'value': valores}
