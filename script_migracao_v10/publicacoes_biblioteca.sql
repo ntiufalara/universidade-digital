36c861aec138247e5dfe5bc443a1097bde25fda5
Diogo <diogocs2@gmail.com>
2018-01-22 08:20:06 -0300

    Atualizando script de migração

--- script_migracao_v10/publicacoes_biblioteca.sql
+++
@@ -1,4 +0,0 @@
-select pb.name, 
-	   pb.ano_pub, 
-	   o.name
-from ud_biblioteca_publicacao as pb, ud_biblioteca_orientador as o, ud_biblioteca_publicacao_orientador_rel as orientador_rel where pb.id = orientador_rel.pub_id and orientador_rel.orientador_id = o.id;
