4ee6c119979df7a510aab692ebbaf050947a483d
Diogo <diogocs2@gmail.com>
2018-03-19 10:04:55 -0300

    Alterando a forma de execução do odoo para o arquivo odoo-bin

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
 # Apache
-rm /var/run/apache2/* -rf
-source /etc/apache2/envvars
-service apache2 start
-tail -f /src/access.log -f /src/error.log
+#rm /var/run/apache2/* -rf
+#source /etc/apache2/envvars
+#service apache2 start
+#tail -f /src/access.log -f /src/error.log
+
+/src/odoo-bin -u all -d ud -r odoo -w odoo --db_host=db
