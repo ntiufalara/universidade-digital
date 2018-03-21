bebbe9bb2cc13cd16abe47ac9fe33c736c23e1d4
Diogo Cabral <diogocs1@gmail.com>
2018-03-20 21:52:28 -0300

    Alterando inicialização do container docker

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,3 @@
 #/bin/bash
 
-# Apache
-rm /var/run/apache2/* -rf
-source /etc/apache2/envvars
-service apache2 start
-tail -f /src/access.log -f /src/error.log
+/src/openerp-server.py -u all -r odoo -w odoo -d ud --db_host=db
