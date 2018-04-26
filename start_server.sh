f030e7711a0badeebcb99eabb8d7e04c9834349d
diogocs1 <dcs1993>
2018-04-26 14:40:25 -0300

    Corrigindo redirecionamento de log do servidor

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-# Apache
-#rm /var/run/apache2/* -rf
-#source /etc/apache2/envvars
-#service apache2 start
-#tail -f /src/access.log -f /src/error.log
-
-/src/odoo-bin -u all -d ud -r odoo -w odoo --db_host=db
+/src/odoo-bin -u all -d ud -r odoo -w odoo --db_host=db > /src/access.log
