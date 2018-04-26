d4b5243b14d2d142854e4d2ed1a8575a5b0ba988
diogocs1 <dcs1993>
2018-04-26 15:04:04 -0300

    Corrigindo script de backup

--- dump_db.sh
+++ dump_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 _now=$(date +"%Y%m%d")
-filename="/db_backup/$_now.backup"
+filename="../db_backup/$_now.backup"
 
-docker-compose exec web pg_dump ud > $filename
+docker-compose exec db pg_dump -Uodoo -wodoo -dud > $filename
