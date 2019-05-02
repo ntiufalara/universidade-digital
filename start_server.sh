248843755c3a8750b66a0b891d0631064e14c174
Diogo Cabral <diogocs1@gmail.com>
2019-05-02 17:22:18 -0300

    Atualizando configuração do odoo para aceitar a rede do host no docker

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/src/odoo-bin -u all -d ud -r odoo -w odoo --logfile=/src/access.log --logrotate
+/src/odoo-bin -u all -d ud -r odoo -w odoo --db_host=127.0.0.1 --logfile=/src/access.log --logrotate
